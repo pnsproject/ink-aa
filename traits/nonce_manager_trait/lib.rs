e9bd1156c839cf0b901c03e11f469dbfc24b68e4
Cupnfish <cupnfish@icloud.com>
2023-06-12 13:29:22 +0800

    add `INonceManager`

--- traits/nonce_manager_trait/lib.rs
+++ traits/nonce_manager_trait/lib.rs
@@ -1,147 +1,40 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-#[ink::contract]
-mod nonce_manager_trait {
+use ink::env::Environment;
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
-    #[ink(storage)]
-    pub struct NonceManagerTrait {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
-    }
-
-    impl NonceManagerTrait {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
-        #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
-        }
-
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
-        #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let nonce_manager_trait = NonceManagerTrait::default();
-            assert_eq!(nonce_manager_trait.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut nonce_manager_trait = NonceManagerTrait::new(false);
-            assert_eq!(nonce_manager_trait.get(), false);
-            nonce_manager_trait.flip();
-            assert_eq!(nonce_manager_trait.get(), true);
-        }
-    }
-
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
+/// INonceManager trait定义了管理nonce（一次性密码）的方法。
+#[ink::trait_definition]
+pub trait INonceManager {
+    /// 获取下一个sender的nonce。
     ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
+    /// 在给定的key内，nonce值是有序的（从零开始，每个userop增加一次）。
+    /// 但是，具有不同key的UserOp可以以任意顺序出现。
+    ///
+    /// # 参数
+    ///
+    /// * `sender` - 账户地址
+    /// * `key` - 高192位的nonce
+    ///
+    /// # 返回值
+    ///
+    /// 返回一个完整的nonce，用于下一个具有该sender的UserOp。
+    #[ink(message)]
+    fn get_nonce(
+        &self,
+        sender: <Self::Env as Environment>::AccountId,
+        key: [u8; 24],
+    ) -> <Self::Env as Environment>::Hash;
 
-        /// A helper function used for calling contract messages.
-        use ink_e2e::build_message;
-
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = NonceManagerTraitRef::default();
-
-            // When
-            let contract_account_id = client
-                .instantiate(
-                    "nonce_manager_trait",
-                    &ink_e2e::alice(),
-                    constructor,
-                    0,
-                    None,
-                )
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            // Then
-            let get = build_message::<NonceManagerTraitRef>(contract_account_id.clone())
-                .call(|nonce_manager_trait| nonce_manager_trait.get());
-            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            Ok(())
-        }
-
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = NonceManagerTraitRef::new(false);
-            let contract_account_id = client
-                .instantiate("nonce_manager_trait", &ink_e2e::bob(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            let get = build_message::<NonceManagerTraitRef>(contract_account_id.clone())
-                .call(|nonce_manager_trait| nonce_manager_trait.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            // When
-            let flip = build_message::<NonceManagerTraitRef>(contract_account_id.clone())
-                .call(|nonce_manager_trait| nonce_manager_trait.flip());
-            let _flip_result = client
-                .call(&ink_e2e::bob(), flip, 0, None)
-                .await
-                .expect("flip failed");
-
-            // Then
-            let get = build_message::<NonceManagerTraitRef>(contract_account_id.clone())
-                .call(|nonce_manager_trait| nonce_manager_trait.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), true));
-
-            Ok(())
-        }
-    }
+    /// 手动增加sender的nonce。
+    ///
+    /// 此方法仅用于完整性，账户不需要在验证期间或其他地方调用它，
+    /// 因为EntryPoint将在任何情况下都会更新nonce。
+    /// 可能的用例是使用各种密钥调用它来“初始化”它们的nonce为1，
+    /// 以便将来的UserOperations不会为具有给定key的第一个交易额外支付费用。
+    ///
+    /// # 参数
+    ///
+    /// * `key` - 高192位的nonce
+    #[ink(message)]
+    fn increment_nonce(&mut self, key: [u8; 24]);
 }
